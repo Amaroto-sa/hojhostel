cbe08a712419fe84a0f200ce20dc14f7cbcb91af
Amaroto-sa <mohammedsillah.uk@gmail.com>
2026-03-29 13:17:41 -0700

    Refine project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
